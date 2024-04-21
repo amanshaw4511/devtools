ee47582aca28069d904ac969ccf918a730a86f82
Aman Shaw <amanshaw4511@protonmail.com>
2024-04-21 23:42:55 +0530

    Add tool menu and search

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import {
-  Autocomplete,
   Box,
   Container,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Paper,
+  Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import "./App.css";
 import { useState } from "react";
-import { Config, configs } from "./transformer";
+import { Config, configs as allConfigs } from "./transformer";
 
 function App() {
-  const defaultConfg = configs[0];
-  const [config, setConfig] = useState<Config>(defaultConfg);
+  const [availableTools, setAvailableTools] = useState<Config[]>(allConfigs);
+
+  const defaultTool = availableTools[0];
+  const [selectedTool, setSelectedTool] = useState<Config>(defaultTool);
 
   const [fromText, setFromText] = useState("");
   const [toText, setToText] = useState("");
   const [error, setError] = useState("");
+  const [searchText, setSearchText] = useState("");
 
   const onConfigChange = async (_: any, newConfig: Config | null) => {
-    const finalConfig = newConfig ?? defaultConfg;
-    setConfig(finalConfig);
+    const finalSelectedTool = newConfig ?? defaultTool;
+    setSelectedTool(finalSelectedTool);
     try {
-      setToText(await finalConfig.method(fromText));
+      setToText(await finalSelectedTool.method(fromText));
       setError("");
     } catch (e) {
       console.error(e);
@@ -32,7 +39,7 @@ function App() {
   const onFromChange = async (n: any) => {
     setFromText(n.target.value);
     try {
-      setToText(await config.method(n.target.value));
+      setToText(await selectedTool.method(n.target.value));
       setError("");
     } catch (e) {
       console.error(e);
@@ -44,38 +51,70 @@ function App() {
     setToText(event.target.value);
   };
 
+  const handleToolsSearch = (searchText: string) => {
+    setSearchText(searchText);
+    setAvailableTools(
+      allConfigs.filter((it) =>
+        it.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+      )
+    );
+  };
+
   return (
     <Container maxWidth="sm" sx={{ m: 4 }}>
-      <Autocomplete
-        options={configs}
-        getOptionLabel={(config) => config.title}
-        value={config}
-        onChange={onConfigChange}
-        renderInput={(params) => <TextField sx={{ width: 500 }} {...params} />}
-      />
-
-      <Typography variant="h5" pt={2} pb={1}>
-        Input <Typography color="red">{error}</Typography>
-      </Typography>
-      <Box>
-        <textarea
-          rows={4}
-          style={{ width: 500 }}
-          onChange={onFromChange}
-          value={fromText}
-        />
-      </Box>
-      <Typography variant="h5" pt={2} pb={1}>
-        Output
-      </Typography>
-      <Box>
-        <textarea
-          rows={4}
-          style={{ width: 500 }}
-          onChange={onToChange}
-          value={toText}
-        />
-      </Box>
+      <Stack direction="row" spacing={2}>
+        <Box sx={{ minWidth: 300, minHeight: 600 }}>
+          <Paper>
+            <TextField
+              label="Search Tools"
+              value={searchText}
+              onChange={(e) => handleToolsSearch(e.target.value)}
+              fullWidth
+            />
+            <MenuList>
+              {availableTools.map((tool) => (
+                <MenuItem
+                  selected={tool.title === selectedTool.title}
+                  key={tool.title}
+                  onClick={() =>
+                    onConfigChange(
+                      null,
+                      availableTools.find((it) => it.title === tool.title) ??
+                        null
+                    )
+                  }
+                >
+                  <ListItemText>{tool.title}</ListItemText>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Paper>
+        </Box>
+        <Box>
+          <Typography variant="h5" pt={2} pb={1}>
+            Input <Typography color="red">{error}</Typography>
+          </Typography>
+          <Box>
+            <textarea
+              rows={4}
+              style={{ width: 500 }}
+              onChange={onFromChange}
+              value={fromText}
+            />
+          </Box>
+          <Typography variant="h5" pt={2} pb={1}>
+            Output
+          </Typography>
+          <Box>
+            <textarea
+              rows={4}
+              style={{ width: 500 }}
+              onChange={onToChange}
+              value={toText}
+            />
+          </Box>
+        </Box>
+      </Stack>
     </Container>
   );
 }
